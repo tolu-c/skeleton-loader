77da47fc9753f202dbcc36a5c4bdd0a37246b5b0
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-08-29 20:02:36 +0100

    second commit

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const dataBody = document.querySelector("[data-body]");
+dataBody.textContent =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates  asperiores atque, perferendis iure sequi aut! Quos, provident assumenda ipsum nobis ratione reprehenderit animi vero maxime odio ad";
+
+const dataTitle = document.querySelector("[data-title]");
+dataTitle.textContent = "Lorem ipsum dolor sit."
+
+console.log(dataTitle)
